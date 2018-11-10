e6f904dca5af58bae2385255635aab1e140f96fe
yangxi <yangxi@cmss.chinamobile.com>
2018-11-10 20:54:41 +0800

    提交试试

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TCBD_SJ
+
+## 数据来源
+http://cbdc.youedata.com/match_subjects/2/show_match
---
+++ data/data_cooker.py
@@ -0,0 +1,8 @@
+# encoding=utf8
+"""
+@author: yang
+@contact: yang9112@gmail.com
+@file: label_cooker.py
+@time: 2018/11/10 20:51
+"""
+
